6f9c897a2a2a0559e6e2ee122b530400f4754686
tamir2017 <897885849@qq.com>
2018-07-18 18:49:08 +0800

    02 angularJS

--- pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.pinyougou.pojo.TbBrand;
 
+import entity.PageResult;
+
 /**
  * 品牌接口
  * @author lenovo
@@ -11,5 +13,50 @@ import com.pinyougou.pojo.TbBrand;
  */
 public interface BrandService {
 
+	/**
+	 * 查询数据库中所有品牌信息
+	 * @return
+	 */
 	public List<TbBrand> findAll();
+	
+	/**
+	 * 品牌分页
+	 * @param pageNum  当前页面
+	 * @param pageSize  每页记录数
+	 * @return
+	 */
+	public PageResult findPage(int pageNum, int pageSize);
+	
+	/**
+	 * 增加品牌
+	 * @param brand
+	 */
+	public void add(TbBrand brand);
+	
+	/**
+	 * 根据ID查询品牌
+	 * @param id
+	 * @return
+	 */
+	public TbBrand findOne(Long id);
+	
+	/**
+	 * 修改品牌
+	 * @param brand
+	 */
+	public void update(TbBrand brand);
+	
+	/**
+	 * 批量删除品牌
+	 * @param ids
+	 */
+	public void delete(Long[] ids);
+	
+	/**
+	 * 品牌分页
+	 * @param pageNum  当前页面
+	 * @param pageSize  每页记录数
+	 * @return
+	 */
+	public PageResult findPage(TbBrand brand,int pageNum, int pageSize);
 }
